6516afa2a8b6a50dfa66d59344b3261a430169bb
mengmeng19 <mengmeng19@staff.weibo.com>
2020-04-30 17:28:45 +0800

    set-cookie

--- server/jsonp/server.js
+++ server/jsonp/server.js
@@ -6,15 +6,18 @@ app.listen(3001, _ => {
 });
 
 app.get('/user/info', (req, res) => {
-    let { callback = Function.prototype } = req.query;
+  // 设置cookie
+  res.header('Set-Cookie', 'SUHB=0HTQlICkedfExt; expires=Friday, 30-Apr-2021 17:24:29 GMT; path=/;domain=aaa.bbb.com;')
+  res.header('Set-Cookie', 'cui=cuimm; expires=Friday, 30-Apr-2021 17:24:29 GMT; path=/;')
+
+  let { callback = Function.prototype } = req.query;
     let data = {
         code: 0,
         user: {
-            name: 'cuimm'
+            name: 'cuimm~'
         }
     }
     res.send(`${callback}(${JSON.stringify(data)})`)
     // JSONP 不安全
     // res.send(`${callback}(document.body.removeChild(document.getElementById('app')))`)
 })
-
